765e6714ce4772de32e905fc6149f7a0c32cd743
izydor <izydor4678@gmail.com>
2025-05-16 13:46:21 +0200

    Add custom fonts

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+export default {
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Manrope', 'ui-sans-serif', 'system-ui'],
+                heading: ['Sora', 'ui-sans-serif', 'system-ui'],
+            },
+        },
+    },
+};
